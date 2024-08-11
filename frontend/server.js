8415f315555d79bc0c6070cf2fc745d02f03ac2e
SUJP123 <sujaypatel04@gmail.com>
2024-08-11 00:29:33 -0400

    install deps

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
 const app = express();
-app.use(express.static(__dirname + '/dist/frontend'));
-app.get('/*', function(req,res) {
-    res.sendFile(__dirname+
-    '/dist/frontend/index.html'); });
+app.search(express.static(__dirname + '/dist/frontend'));
 
 app.listen(process.env.PORT || 8080);
+
+
+app.get('/*', function(req,res) {
+    res.sendFile(__dirname+
+    '/dist/frontend/browser/index.html'); });
+
+
 console.log("running");
