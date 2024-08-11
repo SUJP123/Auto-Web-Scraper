0d3833f55c0e755731f74115022c8ea5709de898
SUJP123 <sujaypatel04@gmail.com>
2024-08-11 01:20:18 -0400

    try old server routing

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const app = express();
-app.search(express.static(__dirname + '/dist/frontend/browser'));
+app.use(express.static(__dirname + '/dist/frontend'));
 
 app.listen(process.env.PORT || 8080);
 
 
 app.get('/*', function(req,res) {
     res.sendFile(__dirname+
-    '/dist/frontend/browser/index.html'); });
+    '/dist/frontend/index.html'); });
 
 
 console.log("running");
