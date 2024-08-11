e0071dd053b4366102d082bdf1bdff4ee3ddfcd6
SUJP123 <sujaypatel04@gmail.com>
2024-08-11 01:45:40 -0400

    fix error

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-app.use(express.static(__dirname + '/dist/browser/frontend'));
+app.use(express.static(__dirname + '/dist/frontend/browser'));
 
 app.listen(process.env.PORT || 8080);
 
