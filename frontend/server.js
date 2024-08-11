2ed5f2c953151f8c07ffa8acb3269e763ce894ad
SUJP123 <sujaypatel04@gmail.com>
2024-08-11 01:22:28 -0400

    update with browser subfolder

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const app = express();
-app.use(express.static(__dirname + '/dist/frontend'));
+app.use(express.static(__dirname + '/dist/browser/frontend'));
 
 app.listen(process.env.PORT || 8080);
 
 
 app.get('/*', function(req,res) {
     res.sendFile(__dirname+
-    '/dist/frontend/index.html'); });
+    '/dist/frontend/browser/index.html'); });
 
 
 console.log("running");
