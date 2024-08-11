daf5d83aea786df40516c644da3332799cf6762f
SUJP123 <sujaypatel04@gmail.com>
2024-08-11 01:01:06 -0400

    comment out base root

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -19,7 +19,7 @@ export const routes: Routes = [
         canActivate: [AuthGuard]
     }
     ,
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    //{ path: '', redirectTo: '/home', pathMatch: 'full' },
 
     
 ];
